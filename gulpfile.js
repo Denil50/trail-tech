fbe3c65c4d565bd9b8b79b8fb1c97e3b9231d0e3
Данила Владимирович <apple@MacBook-Pro.local>
2023-07-13 15:01:19 +0300

    added favicon+3section

--- gulpfile.js
+++ gulpfile.js
@@ -107,6 +107,7 @@ function cleandist(){
 function building() {
     return src([
         'app/css/style.min.css',
+        'app/images/',
         'app/images/*.*',
         'app/fonts/*.*',
         'app/js/main.min.js',
